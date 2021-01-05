00afc426ea41b738cb0d46d55f303118d69fa3f5
Patrick de Carvalho Tavares Rezende Ferreira <patrickctrf@gmail.com>
2021-01-05 01:24:18 -0300

    refactor: New setup.py adaptive to create own requirement file

--- setup.py
+++ setup.py
@@ -2,14 +2,31 @@ import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
-    
-with open("requirements.txt") as file:
-    requirements_txt = file.read().splitlines()
+
+# Se nao houver requirements file, crie com os requisitos
+requirements = 'numpy\ntorch\ntensorflow\ntorchvision\ntorchaudio\n'
+
+try:
+    # Trazemos os requirements atualizados do arquivo txt
+    with open("requirements.txt", "r") as file:
+        requirements_txt = file.read().splitlines()
+except IOError:
+    # Se nao houver arquivo, criamos com o minimo necessario
+    with open("requirements.txt", "w") as file:
+        file.write(requirements)
+except FileNotFoundError:
+    # Se nao houver arquivo, criamos com o minimo necessario
+    with open("requirements.txt", "w") as file:
+        file.write(requirements)
+finally:
+    # Realizamos a leitura do arquivo de uma forma ou outra, para seguir com o setup
+    with open("requirements.txt", "r") as file:
+        requirements_txt = file.read().splitlines()
 
 
 setuptools.setup(
     name="ptk-patrickctrf",
-    version="0.0.1",
+    version="0.0.3",
     author="patrickctrf",
     author_email="patrickctrf@gmail.com",
     description="Package for Python programming by Patrick Ferreira",
@@ -25,3 +42,8 @@ setuptools.setup(
     python_requires='>=3.6',
     install_requires=requirements_txt
 )
+
+# Para gerar uma string do REQUIREMENTS.TXT:
+#with open("requirements.txt", "r") as file:
+    # A funcao abaixo exibe na tela do terminal a string a colocar no inicio DESTE arquivo
+#    file.read()
